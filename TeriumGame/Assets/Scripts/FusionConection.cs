b99ecda070fa0e6dfe1ba11c769f7c2fec1ef641
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2024-03-19 18:13:03 -0500

    photon fusion added

---
+++ TeriumGame/Assets/Scripts/FusionConection.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+using System;
+
+
+namespace GooglyEyesGames.FusionBites
+{
+    public class FusionConection : MonoBehaviour, INetworkRunnerCallbacks
+    {
+
+        public bool connectOnAway = false;
+
+        [HideInInspector]
+        public NetworkRunner runner;
+
+        private void Awake()
+        {
+            if(connectOnAway == true)
+            {
+                ConnectToRunner();
+            }
+        }
+
+
+        public async void ConnectToRunner()
+        {
+            if (runner == null)
+            {
+                runner = gameObject.AddComponent<NetworkRunner>();
+            }
+
+            await runner.StartGame(new StartGameArgs()
+            {
+
+            });
+        }
+
+        public void OnConnectedToServer(NetworkRunner runner)
+        {
+            Debug.Log("Connected server");
+        }
+
+        public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+        {
+          
+        }
+
+        public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+        {
+
+        }
+
+        public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+        {
+
+        }
+
+        public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
+        {
+
+        }
+
+        public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+        {
+
+        }
+
+        public void OnInput(NetworkRunner runner, NetworkInput input)
+        {
+
+        }
+
+        public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+        {
+
+        }
+
+        public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+        {
+
+        }
+
+        public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+        {
+
+        }
+
+        public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+        {
+            Debug.Log("OnPlayerJoined");
+        }
+
+        public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+        {
+
+        }
+
+        public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
+        {
+
+        }
+
+        public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
+        {
+
+        }
+
+        public void OnSceneLoadDone(NetworkRunner runner)
+        {
+
+        }
+
+        public void OnSceneLoadStart(NetworkRunner runner)
+        {
+
+        }
+
+        public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+        {
+
+        }
+
+        public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+        {
+
+        }
+
+        public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+        {
+
+        }
+
+
+    }
+
+  }
+
