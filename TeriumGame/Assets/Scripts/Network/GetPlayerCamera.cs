36311abb5c460f928ae2159d8686bf9aec3566fc
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2024-03-20 01:37:11 -0500

    server established

--- TeriumGame/Assets/Scripts/Network/GetPlayerCamera.cs
+++ TeriumGame/Assets/Scripts/Network/GetPlayerCamera.cs
@@ -2,15 +2,25 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fusion;
+using StarterAssets;
 
 public class GetPlayerCamera : MonoBehaviour
 {
     [SerializeField] Transform playerCameraRoot;
 
 
-    private void Awake()
+
+    private void Start()
     {
-        GameObject virtualCamera = GameObject.Find("PlayerFollowCamera");
-        virtualCamera.GetComponent<CinemachineVirtualCamera>().Follow = playerCameraRoot;
+      NetworkObject thisObject = GetComponent<NetworkObject>();
+
+        if(thisObject.HasStateAuthority)
+        {
+            GameObject virtualCamera = GameObject.Find("PlayerFollowCamera");
+            virtualCamera.GetComponent<CinemachineVirtualCamera>().Follow = playerCameraRoot;
+
+            GetComponent<ThirdPersonController>().enabled = true;
+        }
     }
 }
