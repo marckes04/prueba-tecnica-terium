a30b2df510dcffe4999dc046a61caea27ac429e6
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2024-03-21 11:13:25 -0500

    bullet Added

--- TeriumGame/Assets/Scripts/ThirdPersonController/Scripts/HealthPlayer.cs
+++ TeriumGame/Assets/Scripts/ThirdPersonController/Scripts/HealthPlayer.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static HealthPlayer Instance;
+
+    [SerializeField] private float maxHealth = 100f;
+    [SerializeField] private float currentHealth;
+
+
+    [SerializeField] private Slider healthBar;
+    
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        currentHealth = maxHealth;
+        UpdateHealthBar();
     }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            // If health is zero or less, deactivate player
+            DeactivateScript();
+            Destroy(gameObject);
+        }
+
+        UpdateHealthBar();
+     
+    }
+
+    public void DeactivateScript()
+    {
+        StartCoroutine(DeactivatePlayerGameObject());
+    }
+
+    IEnumerator DeactivatePlayerGameObject()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+    }
+
+    private void UpdateHealthBar()
+    {
+        // Calculate the health percentage
+        float healthPercentage = currentHealth / maxHealth;
+        // Update the slider value
+        healthBar.value = healthPercentage;
+    }
+
 }
